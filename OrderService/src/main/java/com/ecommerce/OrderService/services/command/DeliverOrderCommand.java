619e31c00ee255bb7e1c685d2c87f98618d210f0
Menna Naem <menna@192.168.1.12>
2025-05-15 13:09:36 +0300

    Removed Commentes

--- OrderService/src/main/java/com/ecommerce/OrderService/services/command/DeliverOrderCommand.java
+++ OrderService/src/main/java/com/ecommerce/OrderService/services/command/DeliverOrderCommand.java
@@ -10,25 +10,14 @@ public class DeliverOrderCommand extends OrderCommand {
 
     private final OrderRepository orderRepository;
 
-    private final EmailNotificationObserver emailNotificationObserver;
-
-    // Constructor to inject order and repository
     public DeliverOrderCommand(Order order, OrderRepository orderRepository, EmailNotificationObserver emailNotificationObserver) {
         super(order);
         this.orderRepository = orderRepository;
-        this.emailNotificationObserver = emailNotificationObserver;
     }
 
-    // The execute method contains the logic to deliver the order
     @Override
     public void execute() {
-        // Add any logic required to deliver the order (e.g., notify customer, update delivery details)
-
-        // Update the order status to DELIVERED
         order.setStatus(OrderStatus.DELIVERED);
-
-
-        // Persist the changes to the database
         orderRepository.save(order);
     }
 }
